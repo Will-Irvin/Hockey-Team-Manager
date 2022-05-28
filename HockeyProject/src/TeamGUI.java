d0b4fc850a8dea3c92cba5eb878b105db7fd0834
Will-Irvin <willirvin03@gmail.com>
2022-05-27 21:30:46 -0500

    Added imports for GUI class

--- HockeyProject/src/TeamGUI.java
+++ HockeyProject/src/TeamGUI.java
@@ -1,2 +1,10 @@
-public class TeamGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TeamGUI extends JComponent implements Runnable {
+
+    public void run() {
+
+    }
 }
